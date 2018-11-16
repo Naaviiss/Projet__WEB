97fad1c087be4a4722a05bc2daa2b3c34fb28657
frisco78es <44523139+frisco78es@users.noreply.github.com>
2018-11-16 21:18:42 +0100

    changement de l'emplacement des votes

--- pageprof.php
+++ pageprof.php
@@ -47,7 +47,7 @@
 		
 		
 		#On va aller voir dans chaque fichier
-		$variable="vote-e10";
+		$variable="votes/vote-e10";
 		for ($i=0;$i<99;$i++){
 			if ($i < 10){
 				$file = $variable."0".$i.".txt";
