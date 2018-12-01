2b5539e19f40f034892ef43f68d3f24dbd9df944
nanacy <39811762+nanacy@users.noreply.github.com>
2018-12-01 13:26:11 +0100

    Tab amélioré + ajout fonctions

--- page_admin.php
+++ page_admin.php
@@ -4,6 +4,7 @@
 		header ('Location: deconnexion.php');
 	}
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -15,59 +16,62 @@
 	</head>
 	
 	<header>
-		<?php Include("haut_page.html"); ?>
+		<?php Include("haut_page.html"); ?> 
 	</header>
 
 	<body>
 	<section>
 		<!--Creation du tableau -->
-		<h1 class="txtAdmin">Bonjour <?php echo ucfirst($_SESSION["nom"]);?></br></h1>
+		<h1 class="txtAdmin">Bonjour <?php 
+		echo ucfirst($_SESSION["nom"]);?></br></h1>
 		<p class="pAdmin">Voici un tableau résumant les résultats obtenus grâce aux votes des étudiants</p>
 		<div class="tbl-header">
-			<table id="test" cellpadding="0" cellspacing="0" border="0">
-			<thead>
-				<tr>
 	<?php
-		//tableau avec tous les resultats qu'on enverra ensuite via une session pour le pdf
+		// tableau avec tous les resultats qu'on enverra ensuite via une session pour le pdf
 		$tab = array();
-		
+		$moyecart = array();
+		//pour transmettre les matieres au PDF
+		$_SESSION["matieres"] = $matiere;
+
 		//Création de la première ligne pour l'ensemble des votes
 		$matiere = array ("Mathématiques","Anglais","Programmation","Algorithmique","Economie");
-		$_SESSION["matieres"] = $matiere; //pour transmettre les matieres au PDF
-
-		foreach ($matiere as $lign) {
-				echo "<th><strong>",$lign,"</strong></th>";
-		}
-		echo "				</tr>
-						</thead>
-					</table>
-				</div>
-				<div class='tbl-content'>
-					<table cellpadding='0' cellspacing='0' border='0'>
-						<tbody>"
-							;
-							
+		$notes   = array ("Très satisfait","Satisfait","Moyen","Mécontent","Très mécontent","Sans avis");
+		/*
+		* 0 = "sans avis";
+		* 1 = "Très mécontent";
+		* 2 = "Mécontent";
+		* 3 = "Moyen";
+		* 4 = "Satisfait";
+		* 5 = "Très satisfait";
+		*/
 		
-		//on récupère les connées des votes pour les afficher et calculer la moyenne
-		$nb_file=0; //nombre de fichiers lu
-		$moy=array(0,0,0,0,0); //tableau avec toutes les moyennes
-		$liste_maths=array(); //liste des notes pour chaque matière
+		//On récupère les connées des votes pour les afficher et calculer la moyenne ->		
+		//nombre de fichiers lu
+		$nb_file=0; 
+		//liste des notes pour chaque matière
+		$liste_maths=array(); 
 		$liste_anglais=array();
 		$liste_programmation=array();
 		$liste_algorithme=array();
 		$liste_economie=array();
+		//liste count note pour chaque note(0,1,2...) par matière
+		$liste_maths2=array(0=>0,1=>0,2=>0,3=>0,4=>0,5=>0); 
+		$liste_anglais2=array(0=>0,1=>0,2=>0,3=>0,4=>0,5=>0);
+		$liste_programmation2=array(0=>0,1=>0,2=>0,3=>0,4=>0,5=>0);
+		$liste_algorithme2=array(0=>0,1=>0,2=>0,3=>0,4=>0,5=>0);
+		$liste_economie2=array(0=>0,1=>0,2=>0,3=>0,4=>0,5=>0);
+		// SESSION POUR PDF
+
+		// REMPLIR LES LISTES de notes par matière
 		for($i=1001; $i<1100; $i++){
-			$file = "votes/vote-e" .$i .".txt";
+			$file = "vote-e" .$i .".txt";
 			// Si le fichier existe
 			if (file_exists($file)) {
 				$monfichier = fopen($file, 'r');
 				// Affichage des notes
 				while ( !feof($monfichier) ){
-					echo "<tr>";
 					$dataFile = fgetcsv($monfichier, 0, ";");
 					foreach ($dataFile as $key =>$contenu) {
-						echo "<td>",$contenu,"</td>"; //on affiche la note
-						$moy[$key]= $moy[$key]+$contenu; //pour calculer les moyennes
 						switch($key){ //permet d'associer les notes correspondanes à chaque matière
 							case 0:
 								array_push($liste_maths, $contenu);
@@ -87,84 +91,123 @@
 						}
 						array_push($tab,$contenu);
 					}
-					echo "</tr>";
-					$nb_file = $nb_file + 1; //on incrémente le nombre de fichiers lus
+					$nb_file += 1; //on incrémente le nombre de fichiers lus
 				}
 				fclose($monfichier);
 			}
 		}
+		// CALCUL nb de vote par matiere
+		$liste_note = array($liste_maths, $liste_anglais, $liste_programmation, $liste_algorithme, $liste_economie);
+		$liste_nb_note = array($liste_maths2, $liste_anglais2, $liste_programmation2, $liste_algorithme2, $liste_economie2);
+		for($j=0; $j<5; $j++){
+			$listeN = $liste_note[$j]; // passe sur chaque liste de notes de chaque matière
+			$listeNbNote = $liste_nb_note[$j]; // passe sur chaque liste de notes de chaque matière
+			for($k=0; $k<$nb_file; $k++){
+				$note = $listeN[$k];
+				$listeNbNote[$note] += 1;
+				$liste_nb_note[$j]=$listeNbNote;
+			}
+		}
+		// CALCUL DE LA MOYENNE PAR MATIERE
+		function calculMoy($liste){
+			$arr_size=count($liste); 
+			$moy_liste=array_sum($liste)/$arr_size;
+			return $moy_liste;
+		}
+		// CALCUL ECART TYPE PAR MATIERE
+		function calculEcartType($liste){
+			$arr_size=count($liste); 
+			$moy_liste=array_sum($liste)/$arr_size;
+			$ans=0; 
+			foreach($liste as $element){ 
+				$ans+=pow(($element-$moy_liste),2); 
+			}
+			$variance = $ans/$arr_size; 
+			$ecart_type = sqrt($variance);
+			return $ecart_type;
+		}
 
+		/***********************************************/
+		// AFFICHAGE 1er tableau avec compte des notes
+		/***********************************************/
+		// en tête
+		echo"<table id='test' cellpadding='0' cellspacing='0' border='1'>
+				<tr>";
+		echo "<th></th>";//1ere case vide 
+		foreach ($notes as $lign) {
+			echo "<th><strong>",$lign,"</strong></th>";
+		}
+		echo "	</tr>
+		</div>
+			<div class='tbl-content'>";
+		// table
+		for($j=0; $j<5; $j++){
+			$liste = $liste_nb_note[$j]; // passe sur chaque liste de notes de chaque matière
+			echo"<tr>";
+				echo"<th>".$matiere[$j]."</th>";
+				array_push($tab, $matiere[$j]);
+				for($k=5; $k>=0; $k--){
+					echo"<td>".$liste[$k] ."</td>";
+					array_push($tab, $liste[$k]);
+				}
+			echo"</tr>";
+		}
 		//on créé la session pour la table avec les notes
 		$_SESSION["table"] = $tab;
-		
-		//On ferme le premier tableau
-		echo "      	</tbody>
+		//on ferme le premier tableau
+		echo "
 					</table>
 				</div>
 		</section>";		
-			
-			
-		// Création de l'autre tableau  
-		$moyecart = array();
+
+		/***********************************************/
+		// AFFICHAGE 2eme tableau 
+		/***********************************************/
 		// Pour la moyenne et l'écart type
+		//en tete
 		echo "<section>
 				<!--Creation du tableau -->
-
 				<div class='tbl-header'>
-					<table cellpadding='0' cellspacing='0' border='0'>
+					<table cellpadding='0' cellspacing='0' border='1'>
 						<thead>
 							<tr>
-							<th></th>"; // Ligne vide pour que tout soit aligné
-							
-		//Toujours la ligne avec chaque matière					
+							<th></th>"; // Ligne vide pour que tout soit aligné		
 		foreach ($matiere as $lign) {
 				echo "<th><strong>",$lign,"</strong></th>";
 		}
 		
 		echo "				</tr>
 						</thead>
-					</table>
 				</div>
 				<div class='tbl-content'>
-					<table cellpadding='0' cellspacing='0' border='0'>
 						<tbody>"
 							;
+		//table
+		$liste_matiere = array($liste_maths, $liste_anglais, $liste_programmation, $liste_algorithme, $liste_economie);
 		
-		// CALCUL ET AFFICHAGE DE LA MOYENNE DES NOTES
-		for($i=0; $i<5; $i++){
-			$moy[$i]=$moy[$i]/$nb_file;
-		}
-		// Affiche la moyenne pour chaque matière
+		// AFFICHAGE moyenne
 		echo"<tr>
 			<th>Moyenne</th>";
 			array_push($moyecart,"Moyenne");
 		for($i=0; $i<5; $i++){
-			echo"<td>".$moy[$i]."</td>";
-			array_push($moyecart,$moy[$i]);
+			$moy = calculMoy($liste_matiere[$i]);
+			echo"<td>".$moy."</td>";
+			array_push($moyecart,$moy);
 		}
 		echo"</tr>";
 
-		// calcul
-		$liste_matiere = array($liste_maths, $liste_anglais, $liste_programmation, $liste_algorithme, $liste_economie);
+		// AFFICHAGE ecart type
 		echo"<tr><th>Ecart type</th>";
 		array_push($moyecart,"Ecart-type");
 		for($i=0; $i<5; $i++){
-			$arr_size=count($liste_matiere[$i]); 
-			$moy_liste=array_sum($liste_matiere[$i])/$arr_size;
-			$ans=0; 
-			foreach($liste_matiere[$i] as $element){ 
-				$ans+=pow(($element-$moy_liste),2); 
-			}
-			$variance = $ans/$arr_size; 
-			$ecart_type = sqrt($variance);
-			array_push($moyecart,round($ecart_type,3));
+			$ecart_type = calculEcartType($liste_matiere[$i]);
 			echo"<td>".round($ecart_type,3)."</td>";
 		}
 		echo"</tr>";
 
 		//on envoie le deuxieme tableau au PDF
 		$_SESSION["moyecart"] = $moyecart;
-		
+
 		#On ferme le deuxième tableau 
 		echo "      	</tbody>
 						</table>
