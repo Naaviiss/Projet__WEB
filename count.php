4bb4d6d2fc64a8f48911d3c2d9328100c76844c9
nanacy <39811762+nanacy@users.noreply.github.com>
2018-12-02 16:45:22 +0100

    ajout fonction pour compter les profs

--- count.php
+++ count.php
@@ -10,4 +10,14 @@
 		return $nb_ligne;
 	}
 
+	function nbProf(){
+		$file = "csv/id-profs.csv";
+			if(file_exists($file)) {
+				$tab = file($file);
+				$nb_ligne = count($tab);
+				echo "NOMBREEEEE ---> " .$nb_ligne;
+			}
+		return $nb_ligne;
+	}
+
 ?>  
