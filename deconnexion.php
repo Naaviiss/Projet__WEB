ea6db2709c338e3ddad4c8825c6d2282d33d228f
Wallnya <34969114+Wallnya@users.noreply.github.com>
2019-03-04 13:42:35 +0100

    Correction Nancy

--- deconnexion.php
+++ deconnexion.php
@@ -1,16 +1,12 @@
 <?php
     session_start();
-    if(isset($_SESSION["id"])){
-        unset($_SESSION["id"]);
-        session_destroy();
-    }
     if(isset($_SESSION["nom"])){
         unset($_SESSION["nom"]);
         session_destroy();
     }
-    if(isset($_SESSION["status"])){
-        unset($_SESSION["status"]);
+    if(isset($_SESSION["role"])){
+        unset($_SESSION["role"]);
         session_destroy();
     }
-    header('location: index.html');
+    header('location: index.php');
 ?>
